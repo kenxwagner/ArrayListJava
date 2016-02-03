feff567e10c940cb8a04dbad9e37d471f8b3764f
Ken Wagner <kenwagner@hotmail.com>
2016-02-03 01:05:18 -0800

    Creat kenArrayList

---
+++ KenArrayList.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Arrays;
+public class kenArrayList{
+
+	public static void main(String[] args) {
+	
+	ArrayList arrayListOne;
+	
+	arrayListOne = new ArrayList();
+	
+	ArrayList arrayListTwo - new ArrayList();
+	
+	ArrayList<String> names = new ArrayList<String>();
+	
+	names.add("John Smith");
+	names.add("Mohamed Alami");
+	names.add("Oliver Miller");
+	
+	names.add(2, "Jack Ryan");
+	
+	for (int i = 0; i <names.size(); i++)
+	{
+	System.out.println(names.get(i));
+	}
+	
+	}
+}
+	
